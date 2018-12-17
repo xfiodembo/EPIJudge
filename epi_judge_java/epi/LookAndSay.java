46b795916ea4ecd273196dd3295672318ba3b247
xavier Fiodembo <xfiodembo@gmail.com>
2018-12-16 19:47:58 -0800

    look_and_say

--- epi_judge_java/epi/LookAndSay.java
+++ epi_judge_java/epi/LookAndSay.java
@@ -1,12 +1,35 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class LookAndSay {
   @EpiTest(testDataFile = "look_and_say.tsv")
 
   public static String lookAndSay(int n) {
-    // TODO - you fill in here.
-    return "";
+    String s = "1";
+    for(int i = 1; i < n; i++){
+      s = nextNumber(s);
+    }
+    return s;
+  }
+
+  public static String nextNumber(String s){
+    StringBuilder ret = new StringBuilder();
+    for(int i = 0 ; i< s.length(); i++)
+    {
+      int count = 1;
+      Character test = s.charAt(i);
+      while(i + 1 < s.length() && s.charAt(i + 1) == test){
+        count++;
+        i++;
+      }
+      ret.append(count).append(test);
+    }
+    return ret.toString();
+
   }
 
   public static void main(String[] args) {
