57d24841607d4443ff1981f038e7374bc767a3b1
xavier Fiodembo <xfiodembo@gmail.com>
2018-12-16 16:13:37 -0800

    missing test cases
    and plus one solution

--- epi_judge_java/epi/IntAsArrayIncrement.java
+++ epi_judge_java/epi/IntAsArrayIncrement.java
@@ -5,8 +5,17 @@ import java.util.List;
 public class IntAsArrayIncrement {
   @EpiTest(testDataFile = "int_as_array_increment.tsv")
   public static List<Integer> plusOne(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
+    int n = A.size() -1;
+    A.set(n,A.get(n)+ 1);
+    for(int i = n; i >0 && A.get(i) == 10; --i){
+      A.set(i-1,A.get(i-1) + 1);
+      A.set(i,0);
+    }
+    if(A.get(0) > 9){
+      A.set(0,0);
+      A.add(0,1);
+    }
+    return A;
   }
 
   public static void main(String[] args) {
