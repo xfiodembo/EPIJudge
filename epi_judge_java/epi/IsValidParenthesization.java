f775cfda799d409c90ca577605da2c2151f05779
xavier Fiodembo <xfiodembo@gmail.com>
2018-12-16 21:32:15 -0800

    String isWellFormed: implement using stack and Map<Char,Char>

--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -1,12 +1,26 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+
 public class IsValidParenthesization {
   @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
 
   public static boolean isWellFormed(String s) {
-    // TODO - you fill in here.
-    return true;
+    Map<Character,Character> lookup = Map.of('(',')','[',']','{','}');
+    Stack<Character> stack = new Stack<>();
+    for(int i = 0; i < s.length(); i++){
+      Character test = s.charAt(i);
+      if(!stack.isEmpty() &&  lookup.getOrDefault(stack.peek(),'x') == test ){
+        stack.pop();
+      }else{
+        stack.push(test);
+      }
+    }
+    return stack.empty();
   }
 
   public static void main(String[] args) {
