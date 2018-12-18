725e468e1763b0532891c9d9de1efb5075ba4373
xavier Fiodembo <xfiodembo@gmail.com>
2018-12-17 17:50:41 -0800

    findAnagrams: HashTables!!!!!

--- epi_judge_java/epi/Anagrams.java
+++ epi_judge_java/epi/Anagrams.java
@@ -3,15 +3,35 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.LexicographicalListComparator;
 import epi.test_framework.GenericTest;
-import java.util.Collections;
-import java.util.List;
+
+import java.util.*;
 import java.util.function.BiPredicate;
 public class Anagrams {
   @EpiTest(testDataFile = "anagrams.tsv")
 
   public static List<List<String>> findAnagrams(List<String> dictionary) {
-    // TODO - you fill in here.
-    return null;
+    List<List<String>> ret = new ArrayList<>();
+    HashMap<String,List<String>> map = new HashMap<>();
+    for (String s: dictionary) {
+      char[] arr = s.toCharArray();
+      Arrays.sort(arr);
+      String key = new String(arr);
+      if(!map.containsKey(key)){
+        map.put(key,new ArrayList<>());
+      }
+      if(!map.get(key).contains(s)){
+        map.get(key).add(s);
+      }
+    }
+    for (String k : map.keySet()) {
+      List<String> anagrams = map.get(k);
+      if(anagrams.size() > 1){
+        ret.add(anagrams);
+      }
+
+    }
+
+    return ret;
   }
   @EpiTestComparator
   public static BiPredicate<List<List<String>>, List<List<String>>> comp =
