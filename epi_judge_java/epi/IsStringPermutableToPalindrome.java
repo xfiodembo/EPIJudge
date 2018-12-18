24e9634cfb755eb9520ae67f9c2b614d1d563478
xavier Fiodembo <xfiodembo@gmail.com>
2018-12-18 12:53:04 -0800

    canFormPalindrome: if length is even, all chars must occur an even number of times. if of odd length only one char can occur an odd number of times.
    Hashtables!!!

--- epi_judge_java/epi/IsStringPermutableToPalindrome.java
+++ epi_judge_java/epi/IsStringPermutableToPalindrome.java
@@ -1,12 +1,31 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class IsStringPermutableToPalindrome {
   @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
 
   public static boolean canFormPalindrome(String s) {
-    // TODO - you fill in here.
-    return true;
+    HashMap<Character,Integer> charCounts = new HashMap<>();
+    for(int i = 0; i < s.length(); ++i){
+      Character c = s.charAt(i);
+      charCounts.putIfAbsent(c,0);
+      charCounts.put(c,charCounts.get(c) + 1);
+    }
+
+    int oddOccurringChars = 0;
+
+    for ( Integer count :charCounts.values()) {
+      if(count%2 != 0){
+        ++oddOccurringChars;
+      }
+    }
+    int remainder = s.length()%2;
+    return (remainder == 0 && oddOccurringChars == 0) ||(remainder !=0 && oddOccurringChars == 1) ;
+
   }
 
   public static void main(String[] args) {
